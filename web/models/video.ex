ade72de5e110bd634702c535eb80e961864995e7
Edijs Petersons <eddpeterson@gmail.com>
2016-08-10 23:31:28 +0200

    Added categories to videos

--- web/models/video.ex
+++ web/models/video.ex
@@ -6,16 +6,17 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
 
     timestamps()
   end
-
+  
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:url, :title, :description])
+    |> cast(params, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
   end
 end
